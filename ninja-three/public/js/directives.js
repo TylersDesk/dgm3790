50a5c3e2d21c4509bd498e8e910839b5c949cc0c
TylersDesk <tymayn@gmail.com>
2014-09-11 22:32:29 -0600

    Removed node_modules folder

--- ninja-three/public/js/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var directives = angular.module('myapp.directives', []);
-directives.directive('hello', function () {
-    return {
-        restrict: 'E',
-        template: '<p>Hello from directive</p>'
-    };
-});
